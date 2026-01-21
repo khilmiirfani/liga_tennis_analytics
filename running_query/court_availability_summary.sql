85b40275697b4812511214faaf4a592e4b57a7b7
khilmiirfani <khilmiirfani@gmail.com>
2026-01-21 14:55:08 +0700

    -

---
+++ running_query/court_availability_summary.sql
@@ -0,0 +1,63 @@
+CREATE OR REPLACE VIEW v_court_availability_summary AS
+SELECT 
+    a.item_id AS court_id,
+    
+    -- Calculate hours for Monday
+    SUM(
+        GREATEST(0, TIMESTAMPDIFF(MINUTE, 
+            STR_TO_DATE(CONCAT(a.time_from), '%H:%i'), 
+            STR_TO_DATE(CONCAT(a.time_to),   '%H:%i')
+        )) / 60.0 * a.is_monday
+    ) AS monday_hours,
+
+    -- Calculate hours for Tuesday
+    SUM(
+        GREATEST(0, TIMESTAMPDIFF(MINUTE, 
+            STR_TO_DATE(CONCAT(a.time_from), '%H:%i'), 
+            STR_TO_DATE(CONCAT(a.time_to),   '%H:%i')
+        )) / 60.0 * a.is_tuesday
+    ) AS tuesday_hours,
+
+    -- Calculate hours for Wednesday
+    SUM(
+        GREATEST(0, TIMESTAMPDIFF(MINUTE, 
+            STR_TO_DATE(CONCAT(a.time_from), '%H:%i'), 
+            STR_TO_DATE(CONCAT(a.time_to),   '%H:%i')
+        )) / 60.0 * a.is_wednesday
+    ) AS wednesday_hours,
+
+    -- Calculate hours for Thursday
+    SUM(
+        GREATEST(0, TIMESTAMPDIFF(MINUTE, 
+            STR_TO_DATE(CONCAT(a.time_from), '%H:%i'), 
+            STR_TO_DATE(CONCAT(a.time_to),   '%H:%i')
+        )) / 60.0 * a.is_thursday
+    ) AS thursday_hours,
+
+    -- Calculate hours for Friday
+    SUM(
+        GREATEST(0, TIMESTAMPDIFF(MINUTE, 
+            STR_TO_DATE(CONCAT(a.time_from), '%H:%i'), 
+            STR_TO_DATE(CONCAT(a.time_to),   '%H:%i')
+        )) / 60.0 * a.is_friday
+    ) AS friday_hours,
+
+    -- Calculate hours for Saturday
+    SUM(
+        GREATEST(0, TIMESTAMPDIFF(MINUTE, 
+            STR_TO_DATE(CONCAT(a.time_from), '%H:%i'), 
+            STR_TO_DATE(CONCAT(a.time_to),   '%H:%i')
+        )) / 60.0 * a.is_saturday
+    ) AS saturday_hours,
+
+    -- Calculate hours for Sunday
+    SUM(
+        GREATEST(0, TIMESTAMPDIFF(MINUTE, 
+            STR_TO_DATE(CONCAT(a.time_from), '%H:%i'), 
+            STR_TO_DATE(CONCAT(a.time_to),   '%H:%i')
+        )) / 60.0 * a.is_sunday
+    ) AS sunday_hours
+
+FROM courts_hours_availability a 
+WHERE a.deleted_at IS NULL -- Important: Exclude deleted schedules
+GROUP BY a.item_id;
